db474e76bc82018482a79f21a71d3d07eac2e453
tatuliana <nataliolg@gmail.com>
2018-03-28 00:36:26 -0700

    initial commit

---
+++ app/src/main/java/com/tatuliana/findrepo/GitHubAPI.kt
@@ -0,0 +1,46 @@
+package com.tatuliana.findrepo
+
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+/**
+ * Created by nataliapopova on 3/25/18.
+ */
+interface GitHubService {
+    @GET("search/repositories?")
+    fun searchRepos(@Query("q") searchTerm: String) : Call<GitHubSearchResult>
+
+    @GET("users/{user}/repos")
+    fun userRepos(@Path("user") username: String) : Call<List<Repo>>
+}
+
+class GitHubSearchResult(val items : List<Repo>)
+class Repo(val full_name: String, val owner: GitHubUser, val html_url: String)
+class GitHubUser(val avatar_url: String)
+
+class GitHubRetriever {
+    val service: GitHubService
+    init {
+        val retrofit = Retrofit.Builder().baseUrl("https://api.github.com/").addConverterFactory(GsonConverterFactory.create()).build()
+        service = retrofit.create(GitHubService::class.java)
+    }
+
+    fun searchRepos(callback : Callback<GitHubSearchResult>, searchTerm: String) {
+        var searchT = searchTerm
+        if (searchT == ""){
+            searchT = "Kotlin"
+        }
+        val call = service.searchRepos(searchT)
+        call.enqueue(callback)
+    }
+
+    fun userRepos(callback: Callback<List<Repo>>, username: String) {
+        val call = service.userRepos(username)
+        call.enqueue(callback)
+    }
+}
---
+++ app/src/main/java/com/tatuliana/findrepo/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.tatuliana.findrepo
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val searchEditText = findViewById<EditText>(R.id.searchEditText)
+        val userRepoEditText = findViewById<EditText>(R.id.userRepoEditText)
+
+        val button = findViewById<Button>(R.id.searchButton)
+        button.setOnClickListener {
+            val intent = Intent(this, SearchResultActivity::class.java)
+            intent.putExtra("searchTerm", searchEditText.text.toString())
+            startActivity(intent)
+        }
+
+        val viewRepoButton = findViewById<Button>(R.id.userRepoButton)
+        viewRepoButton.setOnClickListener {
+            val intent = Intent(this, SearchResultActivity::class.java)
+            intent.putExtra("username", userRepoEditText.text.toString())
+            startActivity(intent)
+        }
+    }
+}
